f6884d72764a86f3e98cfc1f7b5e0be0638a2217
Richard Ward <richies@gmail.com>
2022-08-02 13:28:08 +0100

    Add initial tests for string processor service

---
+++ src/test/java/service/StringProcessorServiceTest.java
@@ -0,0 +1,23 @@
+package service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StringProcessorServiceTest {
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void convertDuplicatedStringIntoCompressedString() {
+    }
+
+    @Test
+    void convertCompressedStringIntoDuplicatedString() {
+    }
+}
