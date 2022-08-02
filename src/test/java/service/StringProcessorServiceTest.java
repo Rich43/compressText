c20d410257ff567640ed36d044bbc6af4fb306d3
Richard Ward <richies@gmail.com>
2022-08-02 16:56:47 +0100

    Improve tests

--- src/test/java/service/StringProcessorServiceTest.java
+++ src/test/java/service/StringProcessorServiceTest.java
@@ -12,14 +12,28 @@ class StringProcessorServiceTest {
     public static Stream<Arguments> DuplicatedStringIntoCompressedStringTestData() {
         return Stream.of(
                 Arguments.of("AAAAANNNMMMMYYYYuuuu\\\\\\\\\\\\\\UUUUaaaarWWLLLLJ888DDDDDDDDD", "5A3N4M4Y4u7\\\\4U4ar2W4LJ3\\89D"),
-                Arguments.of(null, null)
+                Arguments.of(null, null),
+                Arguments.of("Hello World", "He2lo World"),
+                Arguments.of("Supercalifragilisticexpialidocious", "Supercalifragilisticexpialidocious"),
+                Arguments.of(
+                        "\u2E69⇂➋☋⍮╻∕ⲯⱲ⾑⠾⾽⣟⽖⤫⸝≫⢯❿\u2FDAⵋ⧧⫎\u2FFDⱢ❄⟾ⅱ⾔☚▷⟲\u2EF9⟝⋞▦ⅸ\u242F┟⮩⣄≂⭘Ⅲ⏋▻Ⳏ⤐⭚ⳣ⻪Ⱙ⪗⦪⎙┉⃬⍳⾑⦍∭⤚⛶⿋™☒⡀⣐\u2FFF⤑⾔Ⓖ⇁⁂⧸⸼♏\u2E79\u20FD⛱\u20C3\u200E⠒⛆⪑⳪⫽ⅇ⟼⫍⏪▄⺵◓⥷⇜⼳┦⃚⫢",
+                        "\u2E69⇂➋☋⍮╻∕ⲯⱲ⾑⠾⾽⣟⽖⤫⸝≫⢯❿\u2FDAⵋ⧧⫎\u2FFDⱢ❄⟾ⅱ⾔☚▷⟲\u2EF9⟝⋞▦ⅸ\u242F┟⮩⣄≂⭘Ⅲ⏋▻Ⳏ⤐⭚ⳣ⻪Ⱙ⪗⦪⎙┉⃬⍳⾑⦍∭⤚⛶⿋™☒⡀⣐\u2FFF⤑⾔Ⓖ⇁⁂⧸⸼♏\u2E79\u20FD⛱\u20C3\u200E⠒⛆⪑⳪⫽ⅇ⟼⫍⏪▄⺵◓⥷⇜⼳┦⃚⫢"
+                ),
+                Arguments.of("◓◓◓◓◓", "5◓")
         );
     }
 
     public static Stream<Arguments> CompressedStringIntoDuplicatedStringTestData() {
         return Stream.of(
                 Arguments.of("5A3N4M4Y4u7\\\\4U4ar2W4LJ3\\89D", "AAAAANNNMMMMYYYYuuuu\\\\\\\\\\\\\\UUUUaaaarWWLLLLJ888DDDDDDDDD"),
-                Arguments.of(null, null)
+                Arguments.of(null, null),
+                Arguments.of("He2lo World", "Hello World"),
+                Arguments.of("Supercalifragilisticexpialidocious", "Supercalifragilisticexpialidocious"),
+                Arguments.of(
+                        "\u2E69⇂➋☋⍮╻∕ⲯⱲ⾑⠾⾽⣟⽖⤫⸝≫⢯❿\u2FDAⵋ⧧⫎\u2FFDⱢ❄⟾ⅱ⾔☚▷⟲\u2EF9⟝⋞▦ⅸ\u242F┟⮩⣄≂⭘Ⅲ⏋▻Ⳏ⤐⭚ⳣ⻪Ⱙ⪗⦪⎙┉⃬⍳⾑⦍∭⤚⛶⿋™☒⡀⣐\u2FFF⤑⾔Ⓖ⇁⁂⧸⸼♏\u2E79\u20FD⛱\u20C3\u200E⠒⛆⪑⳪⫽ⅇ⟼⫍⏪▄⺵◓⥷⇜⼳┦⃚⫢",
+                        "\u2E69⇂➋☋⍮╻∕ⲯⱲ⾑⠾⾽⣟⽖⤫⸝≫⢯❿\u2FDAⵋ⧧⫎\u2FFDⱢ❄⟾ⅱ⾔☚▷⟲\u2EF9⟝⋞▦ⅸ\u242F┟⮩⣄≂⭘Ⅲ⏋▻Ⳏ⤐⭚ⳣ⻪Ⱙ⪗⦪⎙┉⃬⍳⾑⦍∭⤚⛶⿋™☒⡀⣐\u2FFF⤑⾔Ⓖ⇁⁂⧸⸼♏\u2E79\u20FD⛱\u20C3\u200E⠒⛆⪑⳪⫽ⅇ⟼⫍⏪▄⺵◓⥷⇜⼳┦⃚⫢"
+                ),
+                Arguments.of("5◓", "◓◓◓◓◓")
         );
     }
 
