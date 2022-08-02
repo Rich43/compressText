542aab542373925d5ead8d6d45ecacb881165137
Richard Ward <richies@gmail.com>
2022-08-02 16:23:54 +0100

    Implement parameterised tests

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -2,7 +2,8 @@ import service.StringProcessorService;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println(new StringProcessorService().convertDuplicatedStringIntoCompressedString("AAAAANNNMMMMYYYYuuuu\\\\\\\\\\\\\\UUUUaaaarWWLLLLJ888DDDDDDDDD"));
-        System.out.println(new StringProcessorService().convertCompressedStringIntoDuplicatedString("5A3N4M4Y4u7\\\\4U4ar2W4LJ3\\8"));
+        final var stringProcessorService = new StringProcessorService();
+        System.out.println(stringProcessorService.convertDuplicatedStringIntoCompressedString("AAAAANNNMMMMYYYYuuuu\\\\\\\\\\\\\\UUUUaaaarWWLLLLJ888DDDDDDDDD"));
+        System.out.println(stringProcessorService.convertCompressedStringIntoDuplicatedString("5A3N4M4Y4u7\\\\4U4ar2W4LJ3\\8"));
     }
 }
--- src/test/java/service/StringProcessorServiceTest.java
+++ src/test/java/service/StringProcessorServiceTest.java
@@ -2,9 +2,23 @@ package service;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringProcessorServiceTest {
+    public static Stream<Arguments> DuplicatedStringIntoCompressedStringTestData() {
+        return Stream.of(Arguments.of("AAAAANNNMMMMYYYYuuuu\\\\\\\\\\\\\\UUUUaaaarWWLLLLJ888DDDDDDDDD", "5A3N4M4Y4u7\\\\4U4ar2W4LJ3\\8"));
+    }
+
+    public static Stream<Arguments> CompressedStringIntoDuplicatedStringTestData() {
+        return Stream.of(Arguments.of("5A3N4M4Y4u7\\\\4U4ar2W4LJ3\\8", "AAAAANNNMMMMYYYYuuuu\\\\\\\\\\\\\\UUUUaaaarWWLLLLJ888DDDDDDDDD"));
+    }
+
     @BeforeEach
     void setUp() {
     }
@@ -13,11 +27,19 @@ class StringProcessorServiceTest {
     void tearDown() {
     }
 
-    @Test
-    void convertDuplicatedStringIntoCompressedString() {
+    @ParameterizedTest
+    @MethodSource("DuplicatedStringIntoCompressedStringTestData")
+    void convertDuplicatedStringIntoCompressedString(final String input, final String assertion) {
+        final var stringProcessorService = new StringProcessorService();
+        final var result = stringProcessorService.convertDuplicatedStringIntoCompressedString(input);
+        assertEquals(assertion, result);
     }
 
-    @Test
-    void convertCompressedStringIntoDuplicatedString() {
+    @ParameterizedTest
+    @MethodSource("CompressedStringIntoDuplicatedStringTestData")
+    void convertCompressedStringIntoDuplicatedString(final String input, final String assertion) {
+        final var stringProcessorService = new StringProcessorService();
+        final var result = stringProcessorService.convertCompressedStringIntoDuplicatedString(input);
+        assertEquals(assertion, result);
     }
 }
